0acb5fe802c57b803179e8fb8b3610c0bfac339f
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-05-14 21:37:50 -0400

    Mettres seulement les liens vers l'accueil dans le menu

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,14 +1,10 @@
-<nav class="bg-clr-pale-900 p-4">
+<nav class="opacity-75 bg-clr-fonce-900 p-4">
     <div class="flex items-center justify-between">
-        <div class="flex-shrink-0">
-            <a href="#" class="text-clr-mid-100 font-bold">ChapeauChausse</a>
+        <div class="flex-shrink-0 m-7">
+            <a href="/" class="text-clr-mid-100 font-bold text-3xl">ChapeauChaussures</a>
         </div>
         <div class="flex">
             <x-menu-item>Accueil</x-menu-item>
-            <x-menu-item link="utilisateur">Utilisateurs</x-menu-item>
-            <x-menu-item link="produits">Produits</x-menu-item>
-            <x-menu-item link="panier">Panier</x-menu-item>
-            <x-menu-item link="test">Couleurs & exemple panier</x-menu-item>
         </div>
     </div>
 </nav>
