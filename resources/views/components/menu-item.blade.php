d8cc2641ff98d81684aa14071eeae449c461b65d
NabilDjerroud <nabil.djerroud93@hotmail.com>
2024-05-14 21:58:39 -0400

    résolution des conflit

--- resources/views/components/menu-item.blade.php
+++ resources/views/components/menu-item.blade.php
@@ -1,5 +1,4 @@
 <div>
-
     <a href="/{{ $link }}"
         class="font-bold text-3xl leading-6 mr-4 {{ request()->is($link) ? 'text-clr-mid-400 underline' : 'text-clr-mid-100' }}">{{ $slot }}</a>
-</div>
+    </div>
